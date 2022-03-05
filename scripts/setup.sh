49098c1e4244297fa7b0f704eee66933a51390b6
AussieGuy0 <anthony.bruno196@gmail.com>
2022-03-05 17:05:44 +1030

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,17 +5,17 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$CURR_DIR"
 
 DEVPATH="$HOME/dev"
-IDEDIR="$DEVPATH/ide" 
+IDEDIR="$DEVPATH/ide"
 
 TOOLBOX="jetbrains-toolbox-1.19.7784.tar.gz"
 INSYNC="insync_3.5.3.50123-focal_amd64.deb"
 
 # Alacritty
-sudo add-apt-repository ppa:aslatter/ppa 
+sudo add-apt-repository ppa:aslatter/ppa
 
 echo "Updating system"
 sudo apt update
-sudo apt -y upgrade 
+sudo apt -y upgrade
 
 echo "Installing packages"
 cat packages.txt | xargs sudo apt -y install
@@ -24,19 +24,17 @@ sudo snap install spotify
 sudo snap install code --classic
 sudo snap install docker
 
+# Setup SDKMAN
+curl -s "https://get.sdkman.io" | bash
+
 # Setup vim
 echo "Setting up vim"
 
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 vim +PlugInstall
-
-cd ~/.vim/plugged/YouCompleteMe
-python3 install.py --ts-completer --go-completer
-cd "$CURR_DIR"
 
 # Setup ssh key
 echo "Would you like to generate a SSH key? (Y/N)"
